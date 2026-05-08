40652415961b6bddc02709041b1d62668c230f56
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-05-08 21:09:46 +0530

    Update in linked_list_cycle question Done

--- src/linked-list/linked_list_cycle.js
+++ src/linked-list/linked_list_cycle.js
@@ -5,8 +5,8 @@ function hasCycle(head) {
     // Edge case: empty list or single node with no next
     if (head === null || head.next === null) return false;
 
-    let slow = head;       // 🐢 moves 1 step at a time
-    let fast = head;       // 🐇 moves 2 steps at a time
+    let slow = head;       //  moves 1 step at a time
+    let fast = head;       //  moves 2 steps at a time
 
     while (fast !== null && fast.next !== null) {
         slow = slow.next;        // move slow by 1 step

ce0ebce93357bbfd020d7806ddadd24f6e70d3b8
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-03 17:49:21 +0530

    solution added in Array folder added with the name of Perfix-Sum and file also added in that folder with the name of Find Pivot file name

--- src/arrays/prefix-sum/find_pivot.js
+++ src/arrays/prefix-sum/find_pivot.js
@@ -1,1 +1,19 @@
 // 724 question on leetcode
+
+function pivotIndex(nums) {
+    let totalSum = nums.reduce((sum, num) => sum + num, 0);
+
+    let leftSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const rightSum = totalSum - leftSum - nums[i];
+
+        if (leftSum === rightSum) {
+            return i;
+        }
+
+        leftSum += nums[i];
+    }
+
+    return -1;
+}

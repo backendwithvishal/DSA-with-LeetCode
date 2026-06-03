4c76bf16e93af621bedb4dd5ad22e2d6d6bb3e6e
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-03 21:13:41 +0530

    answer (Code) added in Array folder the name of Perfix-Sum new file added with name of Continuous Subarray SumDone

--- src/arrays/prefix-sum/continuous_subarray_sum.js
+++ src/arrays/prefix-sum/continuous_subarray_sum.js
@@ -1,1 +1,29 @@
-// 523 question on leetcode
+// 523 question on leetcode 
+
+function checkSubarraySum(nums, k) {
+    const remainderMap = new Map();
+
+    // remainder 0 occurs before the array starts
+    remainderMap.set(0, -1);
+
+    let prefixSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        prefixSum += nums[i];
+
+        const remainder = prefixSum % k;
+
+        if (remainderMap.has(remainder)) {
+            const prevIndex = remainderMap.get(remainder);
+
+            if (i - prevIndex >= 2) {
+                return true;
+            }
+        } else {
+            // store first occurrence only
+            remainderMap.set(remainder, i);
+        }
+    }
+
+    return false;
+}

157d5a897967bf88d62ff94c44d136c67d8c3548
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-10 00:56:49 +0530

    solution added in sum of two interger number

--- src/binary-search/sumoftwointegers.js
+++ src/binary-search/sumoftwointegers.js
@@ -1,1 +1,19 @@
 // 371 question on Leetcode 
+
+function getSum(a, b) {
+
+    // Repeat until there is no carry
+    while (b !== 0) {
+
+        // Find carry bits
+        const carry = a & b;
+
+        // Add without carry
+        a = a ^ b;
+
+        // Move carry to the next bit
+        b = carry << 1;
+    }
+
+    return a;
+}

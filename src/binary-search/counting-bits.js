8ce94fed34727434fc4cc3201470b80fd5f95f1b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-11 17:52:39 +0530

    In (binary-search) folder add Counting Bits file solution added

--- src/binary-search/counting-bits.js
+++ src/binary-search/counting-bits.js
@@ -1,1 +1,16 @@
 /// 338 question on Leetcode
+
+var countBits = function(n) {
+    // Create an array to store answers from 0 to n
+    const result = new Array(n + 1).fill(0);
+
+    // Calculate the number of 1 bits for each number
+    for (let i = 1; i <= n; i++) {
+        // i >> 1 removes the last bit
+        // i & 1 checks if the last bit is 1
+        result[i] = result[i >> 1] + (i & 1);
+    }
+
+    // Return the array of answers
+    return result;
+};

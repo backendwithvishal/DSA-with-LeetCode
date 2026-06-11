e74b123225ffd8aecbd8ee88f2bcc9408ab50333
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-11 17:36:54 +0530

    Add solution for Number of 1 Bits problem

--- src/binary-search/Number-of-1-bit.js
+++ src/binary-search/Number-of-1-bit.js
@@ -1,1 +1,18 @@
-// 191 Question on Leetcode
+// 191 Question on Leetcode 
+
+var hammingWeight = function(n) {
+    // Count how many 1s are in the binary form of n
+    let count = 0;
+
+    // Keep checking bits until n becomes 0
+    while (n !== 0) {
+        // If the last bit is 1, increase the count
+        count += n & 1;
+
+        // Move to the next bit
+        n >>>= 1;
+    }
+
+    // Return the total number of 1 bits
+    return count;
+};

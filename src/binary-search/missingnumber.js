f53870c7b59a4720ad47eaf6996f02edfaa7d36d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-12 14:45:26 +0530

    Solution added in  Missing Number problem

--- src/binary-search/missingnumber.js
+++ src/binary-search/missingnumber.js
@@ -1,1 +1,14 @@
 // 268 question on Leetcode 
+var missingNumber = function(nums) {
+    // Start with n because numbers should be from 0 to n
+    let result = nums.length;
+
+    // XOR all indexes and all numbers
+    for (let i = 0; i < nums.length; i++) {
+        result ^= i;
+        result ^= nums[i];
+    }
+
+    // The remaining value is the missing number
+    return result;
+};

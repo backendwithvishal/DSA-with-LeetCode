8c68e808620611d1211646de2b1588c0759f0671
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-12 14:57:44 +0530

    Solution added in Reverse bits problem

--- src/binary-search/reverse-bits.js
+++ src/binary-search/reverse-bits.js
@@ -1,1 +1,21 @@
 // 190 question on Leetcode
+
+var reverseBits = function(n) {
+    // Store the reversed number
+    let result = 0;
+
+    // Process all 32 bits
+    for (let i = 0; i < 32; i++) {
+        // Make space for the next bit
+        result <<= 1;
+
+        // Add the last bit of n to result
+        result |= (n & 1);
+
+        // Move n to the next bit
+        n >>>= 1;
+    }
+
+    // Return the reversed bits as an unsigned integer
+    return result >>> 0;
+};

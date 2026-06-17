2fc7ff10e8b16ed3b2b5eefdf2e6452ca294615b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-17 14:55:52 +0530

    solution added  in Combination Sum IV

--- src/dynamic-programming/combinationSumIV.js
+++ src/dynamic-programming/combinationSumIV.js
@@ -1,1 +1,23 @@
 // 377 question on Leetcode
+
+var combinationSum4 = function(nums, target) {
+    // dp[i] = number of ways to make sum i
+    const dp = new Array(target + 1).fill(0);
+
+    // There is 1 way to make sum 0: choose nothing
+    dp[0] = 1;
+
+    // Calculate answers from 1 to target
+    for (let i = 1; i <= target; i++) {
+        // Try every number
+        for (let num of nums) {
+            // If the number can be used
+            if (i >= num) {
+                dp[i] += dp[i - num];
+            }
+        }
+    }
+
+    // Return the number of combinations for target
+    return dp[target];
+};

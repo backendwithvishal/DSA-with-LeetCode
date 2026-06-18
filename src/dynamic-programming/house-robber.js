e3890243e0d2fbbd0611065202a0500210917fd8
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-18 07:19:00 +0530

    solution or code added in the question of House Robber

--- src/dynamic-programming/house-robber.js
+++ src/dynamic-programming/house-robber.js
@@ -1,1 +1,27 @@
 // 198 question on Leetcode
+
+var rob = function(nums) {
+    // Handle small cases
+    if (nums.length === 1) {
+        return nums[0];
+    }
+
+    let robPrev = 0;      // Best amount up to house i-2
+    let robCurrent = 0;   // Best amount up to house i-1
+
+    for (let money of nums) {
+        // Choose the better option:
+        // 1. Rob this house
+        // 2. Skip this house
+        const temp = Math.max(
+            robPrev + money,
+            robCurrent
+        );
+
+        robPrev = robCurrent;
+        robCurrent = temp;
+    }
+
+    // Maximum money we can rob
+    return robCurrent;
+};

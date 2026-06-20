cb048fa69e7e272cea65f83c75ed930d6ac2ff41
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-20 20:52:13 +0530

    solution added in the question of jump-game question

--- src/dynamic-programming/jump-game.js
+++ src/dynamic-programming/jump-game.js
@@ -1,1 +1,20 @@
 // 55 Question on Leetcode
+
+var canJump = function(nums) {
+    // Furthest index we can reach so far
+    let maxReach = 0;
+
+    // Check each position
+    for (let i = 0; i < nums.length; i++) {
+        // If we can't even reach this position
+        if (i > maxReach) {
+            return false;
+        }
+
+        // Update the furthest position we can reach
+        maxReach = Math.max(maxReach, i + nums[i]);
+    }
+
+    // We can reach the last index
+    return true;
+};

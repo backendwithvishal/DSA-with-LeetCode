b8124ec3e42a7f1eac725426389ea50b6394256d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-22 07:44:12 +0530

    Solution added in rotate-image question

--- src/matrix/rotate-image.js
+++ src/matrix/rotate-image.js
@@ -1,1 +1,19 @@
 // 48 question on Leetcode
+
+var rotate = function(matrix) {
+    const n = matrix.length;
+
+    // Step 1: Transpose the matrix
+    // Convert rows into columns
+    for (let row = 0; row < n; row++) {
+        for (let col = row + 1; col < n; col++) {
+            [matrix[row][col], matrix[col][row]] =
+            [matrix[col][row], matrix[row][col]];
+        }
+    }
+
+    // Step 2: Reverse each row
+    for (let row = 0; row < n; row++) {
+        matrix[row].reverse();
+    }
+};

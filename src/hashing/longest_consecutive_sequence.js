39d9c642f72de67c82e6609387d5c3a051b9dbbf
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-28 14:13:46 +0530

    small update in codebase

--- src/hashing/longest_consecutive_sequence.js
+++ src/hashing/longest_consecutive_sequence.js
@@ -1,20 +1,15 @@
 // 128 question on Leetcode
 
 var longestConsecutive = function(nums) {
-    // Store all numbers for O(1) lookup
     let set = new Set(nums);
-
     let longest = 0;
 
     for (let num of set) {
-        // Start only if num is the beginning of a sequence
+        // Only start from the beginning of a sequence
         if (!set.has(num - 1)) {
-            let currentNum = num;
             let length = 1;
 
-            // Count consecutive numbers
-            while (set.has(currentNum + 1)) {
-                currentNum++;
+            while (set.has(num + length)) {
                 length++;
             }
 

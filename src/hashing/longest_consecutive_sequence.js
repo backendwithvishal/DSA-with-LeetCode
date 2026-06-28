4fe447bb57f08cdae0d9ab480bacd729dd35a196
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-28 14:02:15 +0530

    solution added in longest Consecutive sequence question

--- src/hashing/longest_consecutive_sequence.js
+++ src/hashing/longest_consecutive_sequence.js
@@ -1,1 +1,26 @@
 // 128 question on Leetcode
+
+var longestConsecutive = function(nums) {
+    // Store all numbers for O(1) lookup
+    let set = new Set(nums);
+
+    let longest = 0;
+
+    for (let num of set) {
+        // Start only if num is the beginning of a sequence
+        if (!set.has(num - 1)) {
+            let currentNum = num;
+            let length = 1;
+
+            // Count consecutive numbers
+            while (set.has(currentNum + 1)) {
+                currentNum++;
+                length++;
+            }
+
+            longest = Math.max(longest, length);
+        }
+    }
+
+    return longest;
+};

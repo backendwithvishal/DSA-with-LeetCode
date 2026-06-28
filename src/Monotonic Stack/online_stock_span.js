bc4e0d5dc2337d930b6a9c1916edf6ae2384c787
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-28 14:28:21 +0530

    solution added  in Online stock span question

--- src/Monotonic Stack/online_stock_span.js
+++ src/Monotonic Stack/online_stock_span.js
@@ -1,1 +1,24 @@
 // 901 question on Leetcode
+
+var StockSpanner = function() {
+    // Stack will store [price, span]
+    this.stack = [];
+};
+
+StockSpanner.prototype.next = function(price) {
+    let span = 1;
+
+    // Merge all previous prices
+    // that are less than or equal to current price
+    while (
+        this.stack.length > 0 &&
+        this.stack[this.stack.length - 1][0] <= price
+    ) {
+        span += this.stack.pop()[1];
+    }
+
+    // Store current price and its span
+    this.stack.push([price, span]);
+
+    return span;
+};

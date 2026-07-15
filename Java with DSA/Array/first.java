9e897acd4e060c011b3f02fddeadd1b66e38e65d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-16 00:24:28 +0530

    small update in codebase

--- Java with DSA/Array/first.java
+++ Java with DSA/Array/first.java
@@ -1,6 +1,9 @@
-
 public class first{
     public static void main (String [] agrs){
+        int nums [] = {10, 20, 30, 40};
 
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
     }
 }
